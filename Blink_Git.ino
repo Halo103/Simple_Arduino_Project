30d86423208dcdeb6016786e5d848926201ddc08
Aurélien <haloceteki@hotmail.com>
2018-11-01 11:09:46 +0100

    Ajout d'une deuxieme LED en chaine et première compilation

--- Blink_Git.ino
+++ Blink_Git.ino
@@ -16,14 +16,18 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+  // initialize digital pin 5 as an output.
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(6, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);              // wait for a second
+  digitalWrite(6, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);
 }
